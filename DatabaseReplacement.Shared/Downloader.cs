0751ad6801e44c83e81260e5ae91d4be69ca8515
abuzaforfagun <mdabuzaforfagun@gmail.com>
2018-10-01 11:35:34 +0600

    download database with web client

---
+++ DatabaseReplacement.Shared/Downloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace DatabaseReplacement.Shared
+{
+    public class Downloader
+    {
+        private readonly string _fileUrl;
+        private readonly string _destination;
+        private readonly string _fileName;
+
+        public Downloader(string fileUrl, string destination, string fileName)
+        {
+            _fileUrl = fileUrl;
+            _destination = destination;
+            _fileName = fileName;
+        }
+        public void DownloadFile()
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile(_fileUrl, Path.Combine(_destination, _fileName));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+
+        }
+    }
+}
